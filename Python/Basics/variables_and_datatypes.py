f01f69f21b927ac0b2ec4ab18f25feebcfc20f44
StarkDevil2407 <nrajmrvl@gmail.com>
2025-10-07 20:18:35 +0530

    Create variables_and_datatypes.py

---
+++ Python/Basics/variables_and_datatypes.py
@@ -0,0 +1,18 @@
+# Variables and Data Types in Python
+
+# Integer
+age = 20
+
+# Float
+height = 5.9
+
+# String
+name = "Aarav"
+
+# Boolean
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student:", is_student)
