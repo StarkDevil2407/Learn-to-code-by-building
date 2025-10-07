578203f21de379e5289962f83f05e6c8030d8f30
StarkDevil2407 <nrajmrvl@gmail.com>
2025-10-07 20:20:07 +0530

    Create Loops.py

---
+++ Python/Basics/Loops.py
@@ -0,0 +1,11 @@
+# Loops in Python
+
+# For loop example
+for i in range(1, 6):
+    print("For Loop iteration:", i)
+
+# While loop example
+count = 1
+while count <= 5:
+    print("While Loop iteration:", count)
+    count += 1
